535959850369193f54e1e512910fed0ce951305c
HeeYeonKim98 <wow9896@naver.com>
2020-09-07 03:28:01 +0900

    update cors-header

--- jangoproject/info/models.py
+++ jangoproject/info/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Info(models.Model):
+    objects = models.Manager()
     title = models.CharField(max_length=200)
     content = models.TextField()
 
--- jangoproject/jangoproject/settings.py
+++ jangoproject/jangoproject/settings.py
@@ -39,9 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'info',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
